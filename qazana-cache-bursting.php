ac98e8b487e19e1d0eb1c1c393a77f75239c4b04
Franklin Gitonga <frank@radiumthemes.com>
2017-11-02 12:48:19 +0300

    radium_after_generate_css_file action

--- qazana-cache-bursting.php
+++ qazana-cache-bursting.php
@@ -9,10 +9,12 @@ Version: 1.0.0
 License: GPL2
 */
 
-if ( function_exists( 'fvm_purge_all' )) {
+if ( function_exists( 'fvm_purge_all' ) ) {
     add_action( 'qazana/editor/after_save', 'qcb_fvm_purge_all' );
+    add_action( 'radium_after_generate_css_file', 'qcb_fvm_purge_all' );
 } else {
     add_action( 'qazana/editor/after_save', 'qcb_fastvelocity_purge_others' );
+    add_action( 'radium_after_generate_css_file', 'qcb_fastvelocity_purge_others' );
 }
 
 function qcb_fvm_purge_all() {
@@ -23,79 +25,79 @@ function qcb_fvm_purge_all() {
 function qcb_purge_others() {
 
     # wodpress default cache
-    if (function_exists('wp_cache_flush')) {
+    if ( function_exists( 'wp_cache_flush' ) ) {
         wp_cache_flush();
     }
 
     # Purge all W3 Total Cache
-    if (function_exists('w3tc_pgcache_flush')) {
+    if ( function_exists( 'w3tc_pgcache_flush' ) ) {
         w3tc_pgcache_flush();
-        return __('All caches from W3 Total Cache have been purged.');
+        return __( 'All caches from W3 Total Cache have been purged.' );
     }
 
     # Purge WP Super Cache
-    if (function_exists('wp_cache_clear_cache')) {
+    if ( function_exists( 'wp_cache_clear_cache' ) ) {
         wp_cache_clear_cache();
-        return __('All caches from WP Super Cache have been purged.');
+        return __( 'All caches from WP Super Cache have been purged.' );
     }
 
     # Purge WP Rocket
-    if (function_exists('rocket_clean_domain')) {
+    if ( function_exists( 'rocket_clean_domain' ) ) {
         rocket_clean_domain();
-        return __('All caches from WP Rocket have been purged.');
+        return __( 'All caches from WP Rocket have been purged.' );
     }
 
     # Purge Wp Fastest Cache
-    if (isset($GLOBALS['wp_fastest_cache']) && method_exists($GLOBALS['wp_fastest_cache'], 'deleteCache')) {
+    if ( isset( $GLOBALS['wp_fastest_cache']) && method_exists( $GLOBALS['wp_fastest_cache'], 'deleteCache' ) ) {
         $GLOBALS['wp_fastest_cache']->deleteCache();
-        return __('All caches from Wp Fastest Cache have been purged.');
+        return __( 'All caches from Wp Fastest Cache have been purged.' );
     }
 
     # Purge Cachify
-    if (function_exists('cachify_flush_cache')) {
+    if ( function_exists( 'cachify_flush_cache' ) ) {
         cachify_flush_cache();
-        return __('All caches from Cachify have been purged.');
+        return __( 'All caches from Cachify have been purged.' );
     }
 
     # Purge Comet Cache
-    if (class_exists("comet_cache")) {
+    if ( class_exists("comet_cache") ) {
         comet_cache::clear();
-        return __('All caches from Comet Cache have been purged.');
+        return __( 'All caches from Comet Cache have been purged.' );
     }
 
     # Purge Zen Cache
-    if (class_exists("zencache")) {
+    if ( class_exists("zencache") ) {
         zencache::clear();
-        return __('All caches from Sen Cache have been purged.');
+        return __( 'All caches from Sen Cache have been purged.' );
     }
 
     # Purge LiteSpeed Cache 
-    if (class_exists('LiteSpeed_Cache_Tags')) {
-        LiteSpeed_Cache_Tags::add_purge_tag('*');
-        return __('All caches from LiteSpeed Cache have been purged.');
+    if ( class_exists( 'LiteSpeed_Cache_Tags' ) ) {
+        LiteSpeed_Cache_Tags::add_purge_tag( '*' );
+        return __( 'All caches from LiteSpeed Cache have been purged.' );
     }
 
     # Purge SG Optimizer
-    if (function_exists('sg_cachepress_purge_cache')) {
+    if ( function_exists( 'sg_cachepress_purge_cache' ) ) {
         sg_cachepress_purge_cache();
-        return __('All caches from SG Optimizer have been purged.');
+        return __( 'All caches from SG Optimizer have been purged.' );
     }
 
     # Purge Godaddy Managed WordPress Hosting (Varnish + APC)
-    if (class_exists('WPaaS\Plugin')) {
-        qcb_godaddy_request('BAN');
-        return __('All caches from WP Engine have been purged.');
+    if ( class_exists( 'WPaaS\Plugin' ) ) {
+        qcb_godaddy_request( 'BAN' );
+        return __( 'All caches from WP Engine have been purged.' );
     }
 
     # Purge WP Engine
-    if (class_exists("WpeCommon")) {
-        if (method_exists('WpeCommon', 'purge_memcached')) {
+    if ( class_exists("WpeCommon") ) {
+        if ( method_exists( 'WpeCommon', 'purge_memcached' ) ) {
             WpeCommon::purge_memcached();
         }
-        if (method_exists('WpeCommon', 'clear_maxcdn_cache')) {
+        if ( method_exists( 'WpeCommon', 'clear_maxcdn_cache' ) ) {
             WpeCommon::clear_maxcdn_cache();
         }
-        if (method_exists('WpeCommon', 'purge_varnish_cache')) {
+        if ( method_exists( 'WpeCommon', 'purge_varnish_cache' ) ) {
             WpeCommon::purge_varnish_cache();
         }
     }
@@ -109,5 +111,5 @@ function qcb_godaddy_request( $method, $url = null ) {
 	$url  = set_url_scheme( str_replace( $host, WPaas\Plugin::vip(), $url ), 'http' );
 	wp_cache_flush();
 	update_option( 'gd_system_last_cache_flush', time() ); # purge apc
-	wp_remote_request( esc_url_raw( $url ), array('method' => $method, 'blocking' => false, 'headers' => array('Host' => $host)) );
+	wp_remote_request( esc_url_raw( $url ), array( 'method' => $method, 'blocking' => false, 'headers' => array( 'Host' => $host) ) );
 }
